0707a00045ce72e293f991e3c3418c7ac953f6a4
boonyee <neohboonyee99@gmail.com>
2020-10-28 13:57:12 +0800

    complete design of driver application

---
+++ app/src/main/java/com/example/public_transportation_driver_applicaiton/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.public_transportation_driver_applicaiton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginUser(View view) {
+    }
+
+    public void exitApplication(View view) {
+    }
+
+}
--- app/src/main/java/com/example/public_transportation_driver_applicaiton/MainActivity.java
+++ app/src/main/java/com/example/public_transportation_driver_applicaiton/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.public_transportation_driver_applicaiton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        //check the network provider is enabled
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    //get latitude
+                    latitude = location.getLatitude();
+                    //get longitude
+                    longitude = location.getLongitude();
+                }
+            });
+        }
+        else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    //get latitude
+                    latitude = location.getLatitude();
+                    //get longitude
+                    longitude = location.getLongitude();
+
+                }
+            });
+        }
+
+
+
     }
 }
